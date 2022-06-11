43d4ed504925147cd7beac7d5f9d0b407895b553
Malcolm Stone <malcolm.stone91@gmail.com>
2022-06-11 20:13:39 +0100

    add eager loading to home page data access

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @players = Player.all.sort_by { |p| -p.current_score }
+    @players = Player.includes(chomps: [:fruit]).all.sort_by { |p| -p.current_score }
   end
 end
