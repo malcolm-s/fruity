baba6e5b105c20f059b3a9d16cf41b7200614d85
Malcolm Stone <malcolm.stone91@gmail.com>
2022-06-11 21:27:05 +0100

    remove tests for unused resource methods

--- test/controllers/chomps_controller_test.rb
+++ test/controllers/chomps_controller_test.rb
@@ -2,12 +2,8 @@ require "test_helper"
 
 class ChompsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @chomp = chomps(:one)
-  end
-
-  test "should get index" do
-    get chomps_url
-    assert_response :success
+    @player = players(:one)
+    @fruit = fruits(:one)
   end
 
   test "should get new" do
@@ -17,32 +13,9 @@ class ChompsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create chomp" do
     assert_difference("Chomp.count") do
-      post chomps_url, params: { chomp: {  } }
+      post chomps_url, params: { player_id: @player.id, fruit_id: @fruit.id }
     end
 
-    assert_redirected_to chomp_url(Chomp.last)
-  end
-
-  test "should show chomp" do
-    get chomp_url(@chomp)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_chomp_url(@chomp)
-    assert_response :success
-  end
-
-  test "should update chomp" do
-    patch chomp_url(@chomp), params: { chomp: {  } }
-    assert_redirected_to chomp_url(@chomp)
-  end
-
-  test "should destroy chomp" do
-    assert_difference("Chomp.count", -1) do
-      delete chomp_url(@chomp)
-    end
-
-    assert_redirected_to chomps_url
+    assert_redirected_to "/"
   end
 end
--- test/controllers/players_controller_test.rb
+++ test/controllers/players_controller_test.rb
@@ -9,40 +9,4 @@ class PlayersControllerTest < ActionDispatch::IntegrationTest
     get players_url
     assert_response :success
   end
-
-  test "should get new" do
-    get new_player_url
-    assert_response :success
-  end
-
-  test "should create player" do
-    assert_difference("Player.count") do
-      post players_url, params: { player: {  } }
-    end
-
-    assert_redirected_to player_url(Player.last)
-  end
-
-  test "should show player" do
-    get player_url(@player)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_player_url(@player)
-    assert_response :success
-  end
-
-  test "should update player" do
-    patch player_url(@player), params: { player: {  } }
-    assert_redirected_to player_url(@player)
-  end
-
-  test "should destroy player" do
-    assert_difference("Player.count", -1) do
-      delete player_url(@player)
-    end
-
-    assert_redirected_to players_url
-  end
 end
