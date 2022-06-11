d6aed9d5b6a458483fe6bc652b83a314f70d65f6
Malcolm Stone <malcolm.stone91@gmail.com>
2022-06-11 16:37:21 +0100

    refactor controller logic to be simpler and slower i don't care that it's slow there are three records in the db

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,13 +3,13 @@ class HomeController < ApplicationController
     # player_counts = Chomp.joins(:player).group("players.name").count("chomps.id")
     # logger.info("Player counts #{player_counts}")
 
-    @players = Player.all
+    @players = Player.all.sort_by { |p| -p.current_score }
 
-    @player_scores = players.map do |player|
-      score = Chomp.where(player: player).joins(:fruit).sum("fruits.score")
-      logger.info("#{player.name} #{score}")
-      { player: player.name, score: score }
-    end.sort_by { |p| -p[:score] }
+    # @player_scores = @players.map do |player|
+    #   score = Chomp.where(player: player).joins(:fruit).sum("fruits.score")
+    #   logger.info("#{player.name} #{score}")
+    #   { player: player.name, score: score }
+    # end.sort_by { |p| -p[:score] }
 
     logger.info(@player_scores)
   end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,8 @@
 class Player < ApplicationRecord
   has_many :chomps
+
+  def current_score
+    self.chomps.joins(:fruit).sum("fruits.score")
+  end
+
 end
